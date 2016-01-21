4d0594a44f3e71a4f4f392d2b6c4837460e2f90c
vaibhavlella <vaibhavl@buffalo.edu>
2016-01-21 08:05:42 -0500

    Twitter based Movie Rating App, Data Minining

---
+++ initialization.R
@@ -0,0 +1,8 @@
+install.packages("devtools")
+devtools::install_github('rstudio/shinyapps')
+devtools::install_github('rstudio/rscrypt')
+library(shinyapps)
+install.packages(c('ggplot2', 'shiny'))           #removeAuthorizedUser("andy") addAuthorizedUser("andy")
+shinyapps::setAccountInfo(name='vaibhavlella',
+                          token='AF8A8728CE85FA1330825034842BF43F',
+                          secret='k9VY35PJWv6hqX/NA+/+Hbk/11BTaACwOZG+rdzV')
